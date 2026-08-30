0fc18a0fa8c50be227f32d7350329fa02951ca30
madejackson <madejackson@users.noreply.github.com>
2026-08-30 20:17:53 +0000

    fix(ui): licence key input no longer wraps at hyphens

--- client/src/pages/config/configGeneral.jsx
+++ client/src/pages/config/configGeneral.jsx
@@ -229,7 +229,7 @@ const ConfigGeneral = ({ formik, config, status, isAdmin }) => {
             formik={formik}
             multiline
             autoComplete="off"
-            inputProps={{ 'data-1p-ignore': true, 'data-lpignore': 'true', 'data-bwignore': true, style: { fontFamily: 'monospace' } }}
+            inputProps={{ 'data-1p-ignore': true, 'data-lpignore': 'true', 'data-bwignore': true, style: { fontFamily: 'monospace', whiteSpace: 'nowrap', overflowX: 'auto' } }}
             onChange={(e) => {
               formik.setFieldValue("ServerToken", "");
             }}
